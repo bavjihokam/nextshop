387a97367a447295a6ebfb883dda2a7dae759373
ezycode <ezycode@gmail.com>
2024-02-20 09:16:13 +0530

    Prisma-ORM CRUD Operation with MongoDB

---
+++ src/app/(pages)/create/page.jsx
@@ -0,0 +1,82 @@
+"use client";
+import { UpdateUser, allUsers, createUser, fetchUser } from "@/app/libs/crud";
+import { useState, useEffect } from "react";
+function createpage() {
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [userData, setUserData] = useState([]);
+	const handleFormData = async (e) => {
+		e.preventDefault();
+		await createUser({ username: username, email: email });
+	};
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const userData = await fetchUser();
+			setUserData(userData);
+		};
+		getUsers();
+	}, []);
+	return (
+		<div className='max-w-sm mx-auto flex flex-col gap-10 justify-center items-center min-h-screen'>
+			<h1 className='text-4xl text-gray-800'>Creating New user</h1>
+			<div>
+				<form onSubmit={handleFormData}>
+					<div>
+						<label>Username</label>
+						<input
+							type='text'
+							className='outline-none border border-gray-500 p-2 w-full rounded-md'
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+					<div>
+						<label>Email</label>
+						<input
+							type='text'
+							className='outline-none border border-gray-500 p-2 w-full rounded-md'
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<button
+						type='submit'
+						className='px-10 py-1 border border-blue-700 mt-5'
+					>
+						Create User
+					</button>
+				</form>
+			</div>
+			{userData.length >= 1 ? (
+				userData.map((user, i) => {
+					return (
+						<div
+							key={i}
+							className='grid grid-cols-2 gap-4 p-2 border border-gray-400 rounded-md'
+						>
+							<div>Username: {user.name}</div>
+							<div>Email: {user.email} </div>
+						</div>
+					);
+				})
+			) : (
+				<div>
+					{userData.name} - {userData.email}
+				</div>
+			)}
+
+			<div>
+				<form>
+					<button
+						type='submit'
+						formAction={UpdateUser}
+						className='px-10 py-1 border border-blue-500'
+					>
+						Update User
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default createpage;
---
+++ src/app/libs/crud.js
@@ -0,0 +1,84 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+// export const createUser = async () => {
+// 	try {
+// 		const prisma = new PrismaClient();
+// 		const newUsers = [
+// 			{
+// 				email: "User1@gmail.com",
+// 				name: "user1",
+// 			},
+// 			{
+// 				email: "User2@gmail.com",
+// 				name: "user2",
+// 			},
+// 			{
+// 				email: "User3@gmail.com",
+// 				name: "user3",
+// 			},
+// 			{
+// 				email: "User4@gmail.com",
+// 				name: "user4",
+// 			},
+// 		];
+
+// 		const createdUser = await prisma.user.createMany({
+// 			data: newUsers,
+// 		});
+// 		console.log("Created user: ", createdUser);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
+export const createUser = async ({ username: username, email: email }) => {
+	try {
+		const prisma = new PrismaClient();
+		const newUser = { email: email, name: username };
+
+		const createdUser = await prisma.user.create({
+			data: newUser,
+		});
+		console.log("Created user: ", createdUser);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const fetchUser = async () => {
+	try {
+		const prisma = new PrismaClient();
+		// fetch  all users
+		// SQl : select * from tablename where salary >=90000  gt | gte
+		const allUsers = await prisma.user.findUnique({
+			where: {
+				email: "User3@gmail.com",
+			},
+		});
+
+		console.log(allUsers);
+		return allUsers;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const UpdateUser = async () => {
+	try {
+		const prisma = new PrismaClient();
+		await prisma.user.upsert({
+			where: {
+				email: "User2000@gmail.com",
+			},
+			update: {
+				name: "Updated User Name",
+			},
+			create: {
+				email: "User2000@gmail.com",
+				name: "It is New Name",
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
