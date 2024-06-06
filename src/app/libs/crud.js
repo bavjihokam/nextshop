ea17415275f57fe917f9740edd5b2553ef374889
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-06-06 23:54:31 +0530

    update src/app/libs/crud.js

--- src/app/libs/crud.js
+++ src/app/libs/crud.js
@@ -45,7 +45,7 @@ export const createUser = async ({ username: username, email: email }) => {
 	}
 };
 
-export const fetchUser = async () => {
+/*export const fetchUser = async () => {
 	try {
 		const prisma = new PrismaClient();
 		// fetch  all users
@@ -61,7 +61,7 @@ export const fetchUser = async () => {
 	} catch (error) {
 		console.log(error);
 	}
-};
+};*/
 
 export const UpdateUser = async () => {
 	try {
